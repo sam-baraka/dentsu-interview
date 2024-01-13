cd5a6a71fe700e3c27f16a43340e354c9ece3470
Samuel Baraka <sababuvercetti@gmail.com>
2024-01-13 11:20:47 +0300

    Set up routes

---
+++ lib/authentication/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+@RoutePage()
+class LoginPage extends ConsumerWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
