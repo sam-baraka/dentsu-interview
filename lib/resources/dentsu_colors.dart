4c7fe36f692eac57723132c1ee6e5b59a631bb80
Samuel Baraka <sababuvercetti@gmail.com>
2024-01-13 12:45:05 +0300

    Adjusted color

--- lib/resources/dentsu_colors.dart
+++ lib/resources/dentsu_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class DentsuColors {
-  static Color get purple => const Color(0xFF4A2860);
+  static Color get purple => const Color(0xFF863AAC);
   static Color get peach => const Color(0xFFFFE5D3);
   static Color get greyHintColor => const Color(0xFF3F4347);
 }
