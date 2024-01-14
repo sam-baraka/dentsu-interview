5f568d08a4c73e554a58500dfb8e4fe1e5b508de
Samuel Baraka <sababuvercetti@gmail.com>
2024-01-14 08:54:54 +0300

    Started work on the slider

--- lib/resources/dentsu_colors.dart
+++ lib/resources/dentsu_colors.dart
@@ -6,4 +6,6 @@ class DentsuColors {
   static Color get peach => const Color(0xFFFFE5D3);
   static Color get greyHintColor => const Color(0xFF3F4347);
   static Color get lightGrey => const Color(0xFFD0CDCE);
+  static Color get lime => const Color(0xFF5ABEA6);
+  static Color get blueIsh => const Color(0xFF387EA1);
 }
